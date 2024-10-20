09aa9ca5f4c9c0aaaa08414fda1361267fe0fa42
max <me@mclifford.dev>
2024-10-20 22:20:33 +0100

    Added DotNet.fs

---
+++ FBenchmark/ExternalProcesses/DotNet.fs
@@ -0,0 +1,15 @@
+namespace FBenchmark.ExternalProcesses
+
+[<RequireQualifiedAccess>]
+module DotNet =
+    
+    open FsToolbox.ProcessWrappers
+    
+    let build () =
+        
+        
+        DotNet.build ()
+    
+    
+    ()
+
