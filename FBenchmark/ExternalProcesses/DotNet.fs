3fe0dbc09302944cefeca5fb1d454ed5fe6e90d4
max <me@mclifford.dev>
2024-10-23 19:12:33 +0100

    Added processes

--- FBenchmark/ExternalProcesses/DotNet.fs
+++ FBenchmark/ExternalProcesses/DotNet.fs
@@ -1,15 +1,18 @@
 namespace FBenchmark.ExternalProcesses
 
+open FsToolbox.ProcessWrappers
+
 [<RequireQualifiedAccess>]
 module DotNet =
-    
-    open FsToolbox.ProcessWrappers
-    
-    let build () =
-        
-        
-        DotNet.build ()
-    
-    
-    ()
 
+    open FsToolbox.ProcessWrappers
+
+    let buildRelease (dotnetPath: string) (source: string) (output: string) =
+
+        let settings =
+            ({ DotNet.BuildSettings.Default with
+                Path = Some source
+                Output = Some output
+                Configuration = Some DotNet.ConfigurationType.Release })
+
+        DotNet.build startHandler diagnosticHandler dotnetPath settings
--- FBenchmark/ExternalProcesses/Git.fs
+++ FBenchmark/ExternalProcesses/Git.fs
@@ -1,14 +1,31 @@
 namespace FBenchmark.ExternalProcesses
 
+open System.IO
+
 module Git =
     
     open FsToolbox.ProcessWrappers
     
-    let clone () =
+    let clone (gitPath: string) (repositoryUrl: string) (outputDirectory: string) =
         
-        Git.clone 
+        let settings = ({
+            Git.CloneSettings.Default with
+                Repository = repositoryUrl
+                Directory = Some outputDirectory
+        })
         
-        ()
-    
-    ()
+        
+        Git.clone startHandler diagnosticHandler gitPath settings 
+
+    let cloneBranch (gitPath: string) (repositoryUrl: string) (branch: string) (outputDirectory: string) =
+        
+        let settings = ({
+            Git.CloneSettings.Default with
+                Repository = repositoryUrl
+                Directory = Some outputDirectory
+                Branch = Some branch 
+        })
+        
+        
+        Git.clone startHandler diagnosticHandler gitPath settings 
 
